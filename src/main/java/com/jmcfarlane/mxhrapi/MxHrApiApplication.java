bb986b2880514960df546f609ad0aad3cc0991b1
Jeff McFarlane <jmcfarlane.mail@gmail.com>
2021-01-07 20:34:47 -0700

    first commit

---
+++ src/main/java/com/jmcfarlane/mxhrapi/MxHrApiApplication.java
@@ -0,0 +1,13 @@
+package com.jmcfarlane.mxhrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxHrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxHrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmcfarlane/mxhrapi/MxHrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmcfarlane.mxhrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxHrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
